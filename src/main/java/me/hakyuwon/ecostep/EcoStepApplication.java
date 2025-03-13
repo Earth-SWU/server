4536be549dfd306b8a7b8be310676938bce4b15c
hakyuwon <kyu10225@naver.com>
2025-03-13 22:58:05 +0900

    pull

--- src/main/java/me/hakyuwon/ecostep/EcoStepApplication.java
+++ src/main/java/me/hakyuwon/ecostep/EcoStepApplication.java
@@ -2,8 +2,10 @@ package me.hakyuwon.ecostep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "me.hakyuwon.ecostep.repository")
 public class EcoStepApplication {
     public static void main(String[] args) {
         SpringApplication.run(EcoStepApplication.class, args);
