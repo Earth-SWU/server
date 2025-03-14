daab7b39c490bfe79c31dae4968d17a1615756e4
hakyuwon <kyu10225@naver.com>
2025-03-14 19:17:23 +0900

    config 추가

--- src/main/java/me/hakyuwon/ecostep/EcoStepApplication.java
+++ src/main/java/me/hakyuwon/ecostep/EcoStepApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "me.hakyuwon.ecostep.repository")
 public class EcoStepApplication {
     public static void main(String[] args) {
         SpringApplication.run(EcoStepApplication.class, args);
