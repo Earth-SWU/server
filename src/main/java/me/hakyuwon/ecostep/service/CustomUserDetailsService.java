35d0d9a3d92b0486a81e9f22cc2814ce5db1d54c
hakyuwon <kyu10225@naver.com>
2025-03-14 21:53:02 +0900

    mysql

--- src/main/java/me/hakyuwon/ecostep/service/CustomUserDetailsService.java
+++ src/main/java/me/hakyuwon/ecostep/service/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package me.hakyuwon.ecostep.service;
 
 import lombok.RequiredArgsConstructor;
 import me.hakyuwon.ecostep.domain.User;
+import me.hakyuwon.ecostep.dto.UserDto;
 import me.hakyuwon.ecostep.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,7 +16,9 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username)
+        User user = userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return user;
     }
 }
