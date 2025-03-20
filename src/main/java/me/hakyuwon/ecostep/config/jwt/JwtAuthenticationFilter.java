e46db4cb396e86e0ae16e1fcc92268c037603e74
hakyuwon <kyu10225@naver.com>
2025-03-20 23:42:00 +0900

    이메일 인증 제외

--- src/main/java/me/hakyuwon/ecostep/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/me/hakyuwon/ecostep/config/jwt/JwtAuthenticationFilter.java
@@ -7,14 +7,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.hakyuwon.ecostep.repository.UserRepository;
 import me.hakyuwon.ecostep.service.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -85,6 +82,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
         String path = request.getRequestURI();
-        return path.startsWith("/api/users/signup") || path.startsWith("/api/users/login") || path.startsWith("/") ;
+        return path.startsWith("/api/users/signup") || path.startsWith("/api/users/login") || path.startsWith("/") || path.startsWith("/api/email-check") || path.startsWith("/api/verify-code");
     }
 }
