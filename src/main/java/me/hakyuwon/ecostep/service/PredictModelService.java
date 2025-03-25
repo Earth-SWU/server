9b286bbe098b4ea5d1b2dc48e1e0df9c7c126d53
hakyuwon <kyu10225@naver.com>
2025-03-25 09:53:08 +0900

    tumbler mission

--- src/main/java/me/hakyuwon/ecostep/service/PredictModelService.java
+++ src/main/java/me/hakyuwon/ecostep/service/PredictModelService.java
@@ -19,7 +19,7 @@ import java.util.Map;
 public class PredictModelService {
 
     private final RestTemplate restTemplate = new RestTemplate();
-    private static final String FASTAPI_URL = "http://localhost:8000/predict/"; // FastAPI 서버의 URL
+    private static final String FASTAPI_URL = "http://ecostep.co.kr:8000/predict/"; // FastAPI 서버의 URL
 
     public boolean isMissionSuccessful() {
         // API 요청
