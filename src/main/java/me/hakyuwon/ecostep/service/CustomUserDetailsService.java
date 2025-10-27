338b43ca41374156a3a44f6b34c964d829308cd8
hakyuwon <kyu10225@naver.com>
2025-10-28 01:02:30 +0900

    fix: email로 유저 찾기

--- src/main/java/me/hakyuwon/ecostep/service/CustomUserDetailsService.java
+++ src/main/java/me/hakyuwon/ecostep/service/CustomUserDetailsService.java
@@ -16,8 +16,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
         return user;
